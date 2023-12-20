f41e80ad7656fdeb76b6f4874ef9336c71841a64
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-20 19:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-//your JS code here. If required.
+// Your JS code here
+function addBook() {
+    // Get input values
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+    // Validate inputs
+    if (title === '' || author === '' || isbn === '') {
+        alert('Please fill in all fields');
+        return;
+    }
+
+    // Create a new row
+    const tableBody = document.getElementById('book-list');
+    const newRow = tableBody.insertRow();
+
+    // Insert cells
+    const titleCell = newRow.insertCell(0);
+    const authorCell = newRow.insertCell(1);
+    const isbnCell = newRow.insertCell(2);
+    const actionCell = newRow.insertCell(3);
+
+    // Set cell values
+    titleCell.textContent = title;
+    authorCell.textContent = author;
+    isbnCell.textContent = isbn;
+
+    // Add delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete';
+    deleteButton.onclick = function () {
+        tableBody.deleteRow(newRow.rowIndex);
+    };
+
+    actionCell.appendChild(deleteButton);
+
+    // Clear input fields
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
